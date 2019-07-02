26522fdabb2f1058def57019d4777dc165225167
MariaKlykova <maria.klykova@gmail.com>
2019-07-02 16:24:28 +0300

    voice assistant prototype

--- MAIN.py
+++ MAIN.py
@@ -4,31 +4,32 @@ import speech_recognition as sr
 import subprocess as sp
 
 replies = {
-    "hello": "hello, Mr. Do you have any trouble?",
-    "yes": "Do you want to drink?"
-}
+    "yes/no": "Do you want to drink?",
+    "enough": "if you need anything, call me. good bye!"
+}   # сочетания ответ-вопрос
+
+choices = ['yes', 'no']  # варианты ответов пациента
 
 def say(text_to_speech):
     sp.call(['say', text_to_speech])
-say('welcome to voice assistant')
 
-def __main__():
-    rec = sr.Recognizer()
-    mic = sr.Microphone()
-    say(replies['hello'])
-    with mic as source:
-        rec.adjust_for_ambient_noise(source)
-        audio = rec.listen(source)
-        speech_to_text = rec.recognize_google(audio)
-    return speech_to_text
-
-__main__()
-
-while __main__() != 'enough':
-    if __main__() == 'yes':
-        __main__()
-print('break')
+def main():
+    say('hello. welcome to voice assistant. Do you have any trouble? Say yes or no.') # произносится 1 раз в начале
+    while True:  # бесконечный цикл диалога
+        rec = sr.Recognizer()
+        mic = sr.Microphone()
+        with mic as source:   # распознаем ответ с микрофона
+            rec.adjust_for_ambient_noise(source)
+            audio = rec.listen(source)
+            speech_to_text = rec.recognize_google(audio)
+        if speech_to_text in choices:  # сопоставляем ответы со списокм choices
+            say(replies.get('yes/no'))  # если ответ "да" или "нет", цикл продолжаем
+        elif speech_to_text == "enough":  # выход из цикла while
+            say(replies.get("enough"))
+            break
 
 
+if __name__ == '__main__':
+    main()
 
 # return speech_to_text
