ef1186d69f13b6059317a3378348603783efc680
MariaKlykova <maria.klykova@gmail.com>
2019-08-26 21:16:10 +0300

    Exception added

--- server_va.py
+++ server_va.py
@@ -1,14 +1,18 @@
 from flask import Flask, render_template
 from utils import tasks
+import requests
 
 
 app = Flask(__name__)
 @app.route('/')
 def app_va():
-    title = "Запросы от пациентов"
-    subtitle = 'Задачи по запросам пациентов'
-    # !!! добавить обработку исключений, если НЕТ СВЯЗИ С ИНТЕРНЕТОМ
-    return render_template('voice_assist.html', page_title=title, subtitle=subtitle, tasks=tasks)
+    try:
+        title = "Запросы от пациентов"
+        subtitle = 'Задачи по запросам пациентов'
+        return render_template('voice_assist.html', page_title=title, subtitle=subtitle, tasks=tasks)
+    except(requests.RequestException, ConnectionError):
+        print('Подключение к интернету отсуствует')
+        return None
 
 
 if __name__ == "__main__":
